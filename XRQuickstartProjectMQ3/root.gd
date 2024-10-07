75f307142f76b01bcaac210659b21988ac3363d9
James Clarke <james.clarke2@tudublin.ie>
2024-10-07 15:40:01 +0100

    Headset tracking sync code

--- XRQuickstartProjectMQ3/root.gd
+++ XRQuickstartProjectMQ3/root.gd
@@ -1,22 +1,37 @@
 extends Node3D
-
+"""
+This startup code contains a the startup code from the Godot docs (https://docs.godotengine.org/en/stable/tutorials/xr/setting_up_xr.html)
+It also contains code to synchronise the headset orientation at startup, 
+without it the headset usually starts in the incorrect orientation
+"""
 var xr_interface: XRInterface
+@onready var uninitialized_hmd_transform:Transform3D = XRServer.get_hmd_transform()
+var hmd_synchronized:bool = false
 
 func _ready():
 	initXR()
 
 func _process(delta: float) -> void:
-	pass
+	syncHeadsetOrientation()
 
 func initXR():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED) # Turn off v-sync
+		get_viewport().use_xr = true # Change our main viewport to output to the HMD
+		xr_interface.pose_recentered.connect(_on_openxr_pose_recentered)
 		print("OpenXR initialized successfully")
-
-		# Turn off v-sync!
-		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-
-		# Change our main viewport to output to the HMD
-		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized, please check if your headset is connected")
+
+func syncHeadsetOrientation():
+	"""
+	Synchronizes headset ORIENTATION as soon as tracking information begins to arrive :
+	"""
+	if not hmd_synchronized:
+		if uninitialized_hmd_transform != XRServer.get_hmd_transform():
+			hmd_synchronized = true
+			_on_openxr_pose_recentered()
+
+func _on_openxr_pose_recentered() -> void:
+	XRServer.center_on_hmd(XRServer.RESET_BUT_KEEP_TILT, true)
