624c86276b4488a144af1e0eaf53a750f0ca5c14
James Clarke <james.clarke2@tudublin.ie>
2024-10-10 11:04:00 +0100

    XR Tools Pickable Demo

---
+++ XRTools/SampleProjects/PickableTest/README.md
@@ -0,0 +1,1 @@
+Basic scene demonstrating the XRTools "Pickable" functionality
---
+++ XRTools/SampleProjects/PickableTest/node_3d.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+var xr_interface: XRInterface
+@onready var uninitialized_hmd_transform:Transform3D = XRServer.get_hmd_transform()
+var hmd_synchronized:bool = false
+
+func _ready():
+	initXR()
+
+func _process(delta: float) -> void:
+	syncHeadsetOrientation()
+
+func initXR():
+	xr_interface = XRServer.find_interface("OpenXR")
+	if xr_interface and xr_interface.is_initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED) # Turn off v-sync
+		get_viewport().use_xr = true # Change our main viewport to output to the HMD
+		xr_interface.pose_recentered.connect(_on_openxr_pose_recentered)
+		print("OpenXR initialized successfully")
+	else:
+		print("OpenXR not initialized, please check if your headset is connected")
+
+func syncHeadsetOrientation():
+	"""
+	Synchronizes headset ORIENTATION as soon as tracking information begins to arrive :
+	"""
+	if not hmd_synchronized:
+		if uninitialized_hmd_transform != XRServer.get_hmd_transform():
+			hmd_synchronized = true
+			_on_openxr_pose_recentered()
+
+func _on_openxr_pose_recentered() -> void:
+	XRServer.center_on_hmd(XRServer.RESET_BUT_KEEP_TILT, true)
